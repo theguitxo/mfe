69e2d31e2902dbeb5c40e167ea5fa1c8fd5ef46a
Amador (Guitxo) <guitxo@guitxo.com>
2023-12-21 00:32:03 +0100

    añadido micro frontend para cabecera

--- shell/src/app/app.component.ts
+++ shell/src/app/app.component.ts
@@ -2,17 +2,23 @@ import { Component, NgZone } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router, RouterModule } from '@angular/router';
 import { shareNgZone } from '@angular-architects/module-federation-tools';
+import { WrapperComponent, WrapperConfig } from './wrapper.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, WrapperComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'shell';
 
+  headerConfig: WrapperConfig = {
+    remoteName: 'header',
+    exposedModule: './mfe-header',
+    elementName: 'mfe-header'
+  }
   constructor(
     private ngZone: NgZone,
     private readonly router: Router
