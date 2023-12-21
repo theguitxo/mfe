66c57966cd75343a619e320688f6289f15f910b2
Amador (Guitxo) <guitxo@guitxo.com>
2023-12-21 21:37:55 +0100

    nuevo remoto header

---
+++ header/src/bootstrap.ts
@@ -0,0 +1,15 @@
+import { bootstrapApplication, createApplication } from '@angular/platform-browser';
+import { appConfig } from './app/app.config';
+import { AppComponent } from './app/app.component';
+import { createCustomElement } from '@angular/elements';
+
+// bootstrapApplication(AppComponent, appConfig)
+//   .catch((err) => console.error(err));
+
+(async () => {
+  const app = await createApplication(appConfig);
+  const mfeHeader = createCustomElement(AppComponent, {
+    injector: app.injector
+  });
+  customElements.define('mfe-header', mfeHeader);
+})();
